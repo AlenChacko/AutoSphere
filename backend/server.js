7c8a12347a9f2693cb93bb3a8e7e3ea4ab7f263e
Alen Chacko <the.alenchacko@gmail.com>
2025-06-20 20:43:18 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = http.createServer(app);
 // ⬇️ Socket.IO Setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // your frontend
+    origin: "https://auto-sphere-two.vercel.app/", // your frontend
     methods: ["GET", "POST"],
     credentials: true,
   },
